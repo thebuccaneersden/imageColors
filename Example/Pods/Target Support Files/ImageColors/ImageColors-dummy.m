f57bec4b4adf15dd100cb9ca55516a131b9490cc
Robinson Presotto <robipresotto@gmail.com>
2016-03-29 14:11:22 +0100

    Clean repo

--- Example/Pods/Target Support Files/ImageColors/ImageColors-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ImageColors : NSObject
-@end
-@implementation PodsDummy_ImageColors
-@end
--- Example/Pods/Target Support Files/ImageColors/ImageColors-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double ImageColorsVersionNumber;
-FOUNDATION_EXPORT const unsigned char ImageColorsVersionString[];
-
